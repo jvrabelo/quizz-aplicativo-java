1b2b5bf35a24966e1679ffa95eb9f34c4cf13cbd
jvrabelo <106701350+jvrabelo@users.noreply.github.com>
2023-10-10 14:53:46 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,25 +12,25 @@ const timeCount = document.querySelector(".timer .timer_sec");
 
 // if startQuiz button clicked
 start_btn.onclick = ()=>{
-    info_box.classList.add("activeInfo"); //show info box
+info_box.classList.add("activeInfo"); //show info box
 }
 
 // if exitQuiz button clicked
 exit_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
+info_box.classList.remove("activeInfo"); //hide info box
 }
 
 // if continueQuiz button clicked
 continue_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
-    quiz_box.classList.add("activeQuiz"); //show quiz box
-    showQuetions(0); //calling showQestions function
-    queCounter(1); //passing 1 parameter to queCounter
-    startTimer(30); //calling startTimer function
-    startTimerLine(0); //calling startTimerLine function
+info_box.classList.remove("activeInfo"); //hide info box
+quiz_box.classList.add("activeQuiz"); //show quiz box
+showQuetions(0); //calling showQestions function
+queCounter(1); //passing 1 parameter to queCounter
+startTimer(30); //calling startTimer function
+startTimerLine(0); //calling startTimerLine function
 }
 
-let timeValue =  30;
+let timeValue = 30;
 let que_count = 0;
 let que_numb = 1;
 let userScore = 0;
@@ -41,3 +41,21 @@ let widthValue = 0;
 const restart_quiz = result_box.querySelector(".buttons .restart");
 const quit_quiz = result_box.querySelector(".buttons .quit");
 
+// if restartQuiz button clicked
+restart_quiz.onclick = ()=>{
+quiz_box.classList.add("activeQuiz"); //show quiz box
+result_box.classList.remove("activeResult"); //hide result box
+timeValue = 30;
+que_count = 0;
+que_numb = 1;
+userScore = 0;
+widthValue = 0;
+showQuetions(que_count); //calling showQestions function
+queCounter(que_numb); //passing que_numb value to queCounter
+clearInterval(counter); //clear counter
+clearInterval(counterLine); //clear counterLine
+startTimer(timeValue); //calling startTimer function
+startTimerLine(widthValue); //calling startTimerLine function
+timeText.textContent = "Time Left"; //change the text of timeText to Time Left
+next_btn.classList.remove("show"); //hide the next button
+}
