5eb1636ba01d8d55b97b4d51757f524617a6d08e
jvrabelo <106701350+jvrabelo@users.noreply.github.com>
2023-10-10 14:55:25 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,10 +1,10 @@
-// creating an array and passing the number, questions, options, and answers
+// creating an array and passing the number, questions, options and answers
 let questions = [
   {
     numb: 1,
     question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
     answer: "Marie Curiee",
-    options: [
+    option: [
       "Albert Einstein",
       "Marie Curiee",
       "Stephen Hawking",
